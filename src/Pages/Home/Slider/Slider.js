9f7c65728f22276b5514f23cb809f4f48e1522cd
Mazharul Islam <mazharul430@gmail.com>
2022-11-07 20:37:51 -0800

    added footer section

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -3,7 +3,6 @@ import image1 from '../../../assets/img1.jpg';
 import image2 from '../../../assets/img2.jpg';
 import image3 from '../../../assets/img3.jpg';
 import image4 from '../../../assets/img4.jpg';
-import image5 from '../../../assets/img5.jpg';
 import Slide from './Slide';
 
 
@@ -32,14 +31,9 @@ const Slider = () => {
             image: image4,
             prev: 3,
             id: 4,
-            next: 5
-        },
-        {
-            image: image5,
-            prev: 4,
-            id: 5,
             next: 1
-        }
+        },
+       
     ]
 
     return (
